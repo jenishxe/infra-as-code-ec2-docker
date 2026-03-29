61894c2fd3b71c1c546640958ca19aeb61de4704
Jenish XE <jenish.codes@gmail.com>
2026-03-29 15:39:58 +0530

    edited config.sh to clone git repo for Dockerfile

--- config.sh
+++ config.sh
@@ -3,10 +3,15 @@
 apt update -y
 apt install -y docker.io
 
+apt install git -y
+
 systemctl start docker
 systemctl enable docker
 
+git clone https://github.com/jenishxe/infra-as-code-ec2-docker.git
+
+cd infra-as-code-ec2-docker
+
 docker build -t my-webapp .
 
-docker run -d -p 80:80 --restart always 
 docker run -d -p 80:80 --restart always my-webapp
